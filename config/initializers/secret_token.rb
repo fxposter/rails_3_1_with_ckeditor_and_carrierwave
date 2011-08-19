2ead5ee9ac1484c92a592e5347cb57be3a884bb7
Pavel Forkert <fxposter@gmail.com>
2011-08-19 20:41:11 +0300

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rails31WithCkeditorAndCarrierwave::Application.config.secret_token = '1c511077c485cd62435457d907a326c347a8c3082a7e92ff2190ec23cdd7eff1dfeb5e94057da544f8f6f02236fdec0ec00286be221fabc59e772c6f5049479e'
